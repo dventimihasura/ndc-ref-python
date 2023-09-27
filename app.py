0d8f8a9a028b6f43199c7d6bcaac0f81567b5c27
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-09-27 15:52:22 -0700

    Got the jq translator to work

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask
+from flask import request
+import jsonlines
+import jq
 
 app = Flask(__name__)
 
-
-@app.get("/")
-def hello_world():
-    return """
-<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=Y5Xt4PDEXX3vwyDY&autoplay=1&&cc_load_policy=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allow="autoplay" allowfullscreen></iframe>
-    """
+schemas = [schema for schema in jsonlines.open("schema.jsonl")]
+authors = [author for author in jsonlines.open("authors.jsonl")]
+articles = [article for article in jsonlines.open("articles.jsonl")]
+db = {
+    "articles": articles,
+    "authors": authors
+}
 
 
 @app.get("/healthz")
 def healthz():
-    return ("Healthy", 204)
+    return ("", 204)
 
 
 @app.get("/metrics")
 def metrics():
-    return ("", 200)
+    return ("""
+# HELP active_requests number of active requests
+# TYPE active_requests gauge
+active_requests 1
+# HELP total_requests number of total requests
+# TYPE total_requests counter
+total_requests 48
+    """, 200, {"content-type": "text/plain"})
 
 
 @app.get("/capabilities")
@@ -41,220 +52,38 @@ def capabilities():
 
 @app.get("/schema")
 def schema():
-    return {
-        "scalar_types": {
-            "Int": {
-                "aggregate_functions": {
-                    "max": {
-                        "result_type": {
-                            "type": "nullable",
-                            "underlying_type": {
-                                "type": "named",
-                                "name": "Int"
-                            }
-                        }
-                    },
-                    "min": {
-                        "result_type": {
-                            "type": "nullable",
-                            "underlying_type": {
-                                "type": "named",
-                                "name": "Int"
-                            }
-                        }
-                    }
-                },
-                "comparison_operators": {},
-                "update_operators": {}
-            },
-            "String": {
-                "aggregate_functions": {},
-                "comparison_operators": {
-                    "like": {
-                        "argument_type": {
-                            "type": "named",
-                            "name": "String"
-                        }
-                    }
-                },
-                "update_operators": {}
-            }
-        },
-        "object_types": {
-            "article": {
-                "description": "An article",
-                "fields": {
-                    "author_id": {
-                        "description": "The article's author ID",
-                        "type": {
-                            "type": "named",
-                            "name": "Int"
-                        }
-                    },
-                    "id": {
-                        "description": "The article's primary key",
-                        "type": {
-                            "type": "named",
-                            "name": "Int"
-                        }
-                    },
-                    "title": {
-                        "description": "The article's title",
-                        "type": {
-                            "type": "named",
-                            "name": "String"
-                        }
-                    }
-                }
-            },
-            "author": {
-                "description": "An author",
-                "fields": {
-                    "first_name": {
-                        "description": "The author's first name",
-                        "type": {
-                            "type": "named",
-                            "name": "String"
-                        }
-                    },
-                    "id": {
-                        "description": "The author's primary key",
-                        "type": {
-                            "type": "named",
-                            "name": "Int"
-                        }
-                    },
-                    "last_name": {
-                        "description": "The author's last name",
-                        "type": {
-                            "type": "named",
-                            "name": "String"
-                        }
-                    }
-                }
-            }
-        },
-        "collections": [
-            {
-                "name": "articles",
-                "description": "A collection of articles",
-                "arguments": {},
-                "type": "article",
-                "deletable": False,
-                "uniqueness_constraints": {
-                    "ArticleByID": {
-                        "unique_columns": [
-                            "id"
-                        ]
-                    }
-                },
-                "foreign_keys": {}
-            },
-            {
-                "name": "authors",
-                "description": "A collection of authors",
-                "arguments": {},
-                "type": "author",
-                "deletable": False,
-                "uniqueness_constraints": {
-                    "AuthorByID": {
-                        "unique_columns": [
-                            "id"
-                        ]
-                    }
-                },
-                "foreign_keys": {}
-            },
-            {
-                "name": "articles_by_author",
-                "description": "Articles parameterized by author",
-                "arguments": {
-                    "author_id": {
-                        "type": {
-                            "type": "named",
-                            "name": "Int"
-                        }
-                    }
-                },
-                "type": "article",
-                "deletable": False,
-                "uniqueness_constraints": {},
-                "foreign_keys": {}
-            }
-        ],
-        "functions": [
-            {
-                "name": "latest_article_id",
-                "description": "Get the ID of the most recent article",
-                "arguments": {},
-                "result_type": {
-                    "type": "nullable",
-                    "underlying_type": {
-                        "type": "named",
-                        "name": "Int"
-                    }
-                }
-            }
-        ],
-        "procedures": [
-            {
-                "name": "upsert_article",
-                "description": "Insert or update an article",
-                "arguments": {
-                    "article": {
-                        "description": "The article to insert or update",
-                        "type": {
-                            "type": "named",
-                            "name": "article"
-                        }
-                    }
-                },
-                "result_type": {
-                    "type": "Noneable",
-                    "underlying_type": {
-                        "type": "named",
-                        "name": "article"
-                    }
-                }
-            }
-        ]
-    }
+    for schema in schemas:
+        return schema
 
 
 @app.post("/query")
 def query():
-    return [
-        {
-            "rows": [
-                {
-                    "ine_": {
-                        "title": "Row Field Value"
-                    },
-                    "elit4": {
-                        "title": "Row Field Value"
-                    }
-                },
-                {
-                    "est_ae": {
-                        "title": "Row Field Value"
-                    },
-                    "commodo_16b": {
-                        "title": "Row Field Value"
-                    }
-                }
-            ],
-            "aggregates": None,
-            "dolor__13": "veniam",
-            "dolor32": -44004488.342306614
-        }
-    ]
+    for filter in jq.all(r'''
+{
+  "collection":
+    (
+      .collection
+    ),
+  "filter":
+    (
+      [
+        .query.fields
+        |to_entries[]
+        |select(.value.type=="column")
+        |{"key": .key, "value": ".\(.value.column)"}
+      ]
+      |map("\"\(.key)\": \(.value)")
+      |join(", ")
+    )
+}
+|".\(.collection)[]|{\(.filter)}"
+    ''', request.get_json()):
+        return jq.all(filter, db)
 
 
 @app.post("/explain")
 def explain():
     return {
-        "details": {
-            "proident_6": "velit ut in qui",
-            "voluptate_42": "sint ipsum cillum ea reprehenderit"
-        }
+        "message": "explain is not supported",
+        "details": None
     }
